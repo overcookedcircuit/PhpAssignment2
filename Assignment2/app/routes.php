ec7bad182bfeec1c04af2e6ab1a465948df871ea
overcookedcircuit <manaspatel31122004@gmail.com>
2024-03-18 00:16:43 -0400

    renamed routes

--- Assignment2/app/routes.php
+++ Assignment2/app/routes.php
@@ -2,5 +2,5 @@
 $this->addRoute('User/register', 'User,register');
 $this->addRoute('User/login', 'User,login');
 $this->addRoute('Profile/create', 'Profile,create');
-$this->addRoute('Profile/view', 'Profile,index');
-$this->addRoute('Profile/edit', 'Profile,modify');
+$this->addRoute('Profile/profileView', 'Profile,index');
+$this->addRoute('Profile/profileEdit', 'Profile,modify');
