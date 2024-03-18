ad8fe87ec4725b30bc661847fed76bb497b0f85a
overcookedcircuit <manaspatel31122004@gmail.com>
2024-03-17 22:13:51 -0400

    profile edit and profile view page

---
+++ Assignment2/app/views/Profile/profileView.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title><?= $name ?> view</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<header>
+    <h1 class="display-1">Profile</h1>
+</header>
+<body>
+    <?php
+    echo "$profile->first_name $profile->middle_name $profile->last_name";
+    ?>
+</body>
+</html>
