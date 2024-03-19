7dfeb9e4b11c0c2bfe695014cd72cfeb6cad27a5
overcookedcircuit <manaspatel31122004@gmail.com>
2024-03-19 00:09:30 -0400

    better view

--- Assignment2/app/views/Profile/profileView.php
+++ Assignment2/app/views/Profile/profileView.php
@@ -5,10 +5,62 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
-<header>
-    <h1 class="display-1">Profile</h1>
+<style>
+	body {
+    font-family: Arial, sans-serif;
+  }
+  .header {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    background-color: #f4f4f4;
+  }
+  .logo {
+    margin-right: 20px;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: black;
+  }
+  .logo img {
+    height: 50px;
+    margin-right: 10px;
+  }
+  .search-bar {
+    margin-right: 10px;
+    display: flex;
+    align-items: center;
+  }
+  .search-bar input {
+    padding: 5px;
+  }
+  .buttons {
+    display: flex;
+  }
+  .buttons button {
+    margin-left: 5px;
+    padding: 5px;
+  }
+</style>
+<header class="header">
+  <a href="#" class="logo">
+    <img src="logo.png" alt="Logo">
+    Instasham
+  </a>
+  <div class="search-bar">
+    <input type="text" placeholder="Search...">
+    <button title="search">🔍</button>
+  </div>
+  <div class="buttons">
+    <button title="Log in">1️⃣</button>
+    <button title="New Publication">➕</button>
+    <button title="My Profile">👤</button>
+  </div>
 </header>
 <body>
+	<div>
+	<h1 class="display-1">Profile</h1>
+	</div>
 <div class='container'>
 		<h1>User profile</h1>
 		<dl>
